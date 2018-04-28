b208c1accaa3c38afbfc7fad5ecd89326eef228b
David Chiu <davidwchiu@gmail.com>
2018-04-28 14:12:53 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
-    if !(BRANDS.includes?(brands)) then
+    if !(BRANDS.includes?(brand)) then
       BRANDS << brands
   end
 end
