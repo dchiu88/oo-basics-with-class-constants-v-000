61850b86f50b4679a819893e50d541214c9bc841
David Chiu <davidwchiu@gmail.com>
2018-04-28 14:09:49 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,25 +1,16 @@
-
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+
   BRANDS = []
-   def initialize(brand)
-     @brand = brand
-     brands = @brand
 
-        if  !(BRANDS.include?(brands)) then
-          BRANDS << brands
-        end
-
+  def initialize(brand)
+    @brand = brand
   end
+
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
 end
-  brands = ["Uggs", "Rainbow", "Nike", "Nike"]
-      # brands.each do |brand|
-      #   Shoe.new(brand)
-      # end
-my_shoe=Shoe.new(brands)
-# my_shoe.length
